29de0458da7f7e439cff64a17961333ac57c87ba
Lexinor <alternonolimit@gmail.com>
2022-09-12 13:18:29 +0200

    Updated SQL

--- esx.sql
+++ esx.sql
@@ -15,6 +15,7 @@ CREATE TABLE `users` (
 	`job_grade` INT NULL DEFAULT 0,
 	`faction` varchar(30) COLLATE utf8mb4_bin DEFAULT 'nofaction',
 	`faction_grade` int(11) DEFAULT 0,
+	`loadout` LONGTEXT NULL DEFAULT NULL,
 	`position` VARCHAR(255) NOT NULL DEFAULT '{"x":-1042.9266,"y":-2746.4329,"z":21.3594,"heading":326.3846}',
 	`firstname` VARCHAR(16) NULL DEFAULT NULL,
 	`lastname` VARCHAR(16) NULL DEFAULT NULL,
@@ -22,12 +23,14 @@ CREATE TABLE `users` (
 	`sex` VARCHAR(1) NULL DEFAULT NULL,
 	`height` INT NULL DEFAULT NULL,
 	`skin` LONGTEXT NULL DEFAULT NULL,
-	`phone_number` VARCHAR(20) DEFAULT NULL,
 	`status` LONGTEXT NULL,
-	`loadout` LONGTEXT NULL DEFAULT NULL,
-	`last_property` VARCHAR(255) NULL,
 	`is_dead` TINYINT(1) NULL DEFAULT '0',
+	`phone_number` VARCHAR(20) DEFAULT NULL,		
+	`id` int(11) NOT NULL,
 	`disabled` TINYINT(1) NULL DEFAULT '0',
+	`last_property` varchar(255) DEFAULT NULL,
+	`created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+	`last_seen` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
 	`last_connection` varchar(20) DEFAULT NULL,
 
 	PRIMARY KEY (`identifier`)
@@ -164,7 +167,7 @@ CREATE TABLE `user_licenses` (
 
 	PRIMARY KEY (`id`),
 	INDEX `owner` (`owner`)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `society_moneywash` (
 	`id` INT NOT NULL AUTO_INCREMENT,
@@ -173,7 +176,7 @@ CREATE TABLE `society_moneywash` (
 	`amount` INT NOT NULL,
 
 	PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `addon_account` (
 	`name` VARCHAR(60) NOT NULL,
@@ -191,9 +194,8 @@ INSERT INTO `addon_account` (name, label, shared) VALUES
 	('society_cardealer', 'Concessionnaire', 1),
 	('society_banker', 'Banque', 1),
 	('society_realestateagent', 'Agence Immobilière', 1),
-	('bank_savings', 'Bank Savings', 0),
-	('property_black_money', 'Money Sale Property', 0)
-;
+	('bank_savings', 'Bank Savings', 0)
+	;
 
 CREATE TABLE `addon_account_data` (
 	`id` INT NOT NULL AUTO_INCREMENT,
@@ -220,7 +222,6 @@ INSERT INTO `addon_inventory` (name, label, shared) VALUES
 	('society_police', 'Police', 1),
 	('society_taxi', 'Taxi', 1),
 	('society_cardealer', 'Cardealer', 1),
-	('property', 'Property', 0)
 ;
 
 CREATE TABLE `addon_inventory_items` (
@@ -262,80 +263,6 @@ CREATE TABLE `datastore_data` (
 	UNIQUE INDEX `index_datastore_data_name_owner` (`name`, `owner`),
 	INDEX `index_datastore_data_name` (`name`)
 );
-
-CREATE TABLE `owned_properties` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(255) NOT NULL,
-	`price` DOUBLE NOT NULL,
-	`rented` INT(11) NOT NULL,
-	`owner` VARCHAR(60) NOT NULL,
-
-	PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `properties` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(255) DEFAULT NULL,
-	`label` VARCHAR(255) DEFAULT NULL,
-	`entering` VARCHAR(255) DEFAULT NULL,
-	`exit` VARCHAR(255) DEFAULT NULL,
-	`inside` VARCHAR(255) DEFAULT NULL,
-	`outside` VARCHAR(255) DEFAULT NULL,
-	`ipls` VARCHAR(255) DEFAULT '[]',
-	`gateway` VARCHAR(255) DEFAULT NULL,
-	`is_single` INT(11) DEFAULT NULL,
-	`is_room` INT(11) DEFAULT NULL,
-	`is_gateway` INT(11) DEFAULT NULL,
-	`room_menu` VARCHAR(255) DEFAULT NULL,
-	`price` INT(11) NOT NULL,
-
-	PRIMARY KEY (`id`)
-);
-
-INSERT INTO `properties` VALUES
-	(1, 'WhispymoundDrive', '2677 Whispymound Drive', '{\"y\":564.89, \"z\":182.959, \"x\":119.384}', '{\"x\":117.347, \"y\":559.506, \"z\":183.304}', '{\"y\":557.032, \"z\":183.301, \"x\":118.037}', '{\"y\":567.798, \"z\":182.131, \"x\":119.249}', '[]', NULL, 1, 1, 0, '{\"x\":118.748, \"y\":566.573, \"z\":175.697}', 1500000),
-	(2, 'NorthConkerAvenue2045', '2045 North Conker Avenue', '{\"x\":372.796, \"y\":428.327, \"z\":144.685}', '{\"x\":373.548, \"y\":422.982, \"z\":144.907}', '{\"y\":420.075, \"z\":145.904, \"x\":372.161}', '{\"x\":372.454, \"y\":432.886, \"z\":143.443}', '[]', NULL, 1, 1, 0, '{\"x\":377.349, \"y\":429.422, \"z\":137.3}', 1500000),
-	(3, 'RichardMajesticApt2', 'Richard Majestic, Apt 2', '{\"y\":-379.165, \"z\":37.961, \"x\":-936.363}', '{\"y\":-365.476, \"z\":113.274, \"x\":-913.097}', '{\"y\":-367.637, \"z\":113.274, \"x\":-918.022}', '{\"y\":-382.023, \"z\":37.961, \"x\":-943.626}', '[]', NULL, 1, 1, 0, '{\"x\":-927.554, \"y\":-377.744, \"z\":112.674}', 1700000),
-	(4, 'NorthConkerAvenue2044', '2044 North Conker Avenue', '{\"y\":440.8, \"z\":146.702, \"x\":346.964}', '{\"y\":437.456, \"z\":148.394, \"x\":341.683}', '{\"y\":435.626, \"z\":148.394, \"x\":339.595}', '{\"x\":350.535, \"y\":443.329, \"z\":145.764}', '[]', NULL, 1, 1, 0, '{\"x\":337.726, \"y\":436.985, \"z\":140.77}', 1500000),
-	(5, 'WildOatsDrive', '3655 Wild Oats Drive', '{\"y\":502.696, \"z\":136.421, \"x\":-176.003}', '{\"y\":497.817, \"z\":136.653, \"x\":-174.349}', '{\"y\":495.069, \"z\":136.666, \"x\":-173.331}', '{\"y\":506.412, \"z\":135.0664, \"x\":-177.927}', '[]', NULL, 1, 1, 0, '{\"x\":-174.725, \"y\":493.095, \"z\":129.043}', 1500000),
-	(6, 'HillcrestAvenue2862', '2862 Hillcrest Avenue', '{\"y\":596.58, \"z\":142.641, \"x\":-686.554}', '{\"y\":591.988, \"z\":144.392, \"x\":-681.728}', '{\"y\":590.608, \"z\":144.392, \"x\":-680.124}', '{\"y\":599.019, \"z\":142.059, \"x\":-689.492}', '[]', NULL, 1, 1, 0, '{\"x\":-680.46, \"y\":588.6, \"z\":136.769}', 1500000),
-	(7, 'LowEndApartment', 'Basic apartment', '{\"y\":-1078.735, \"z\":28.4031, \"x\":292.528}', '{\"y\":-1007.152, \"z\":-102.002, \"x\":265.845}', '{\"y\":-1002.802, \"z\":-100.008, \"x\":265.307}', '{\"y\":-1078.669, \"z\":28.401, \"x\":296.738}', '[]', NULL, 1, 1, 0, '{\"x\":265.916, \"y\":-999.38, \"z\":-100.008}', 562500),
-	(8, 'MadWayneThunder', '2113 Mad Wayne Thunder', '{\"y\":454.955, \"z\":96.462, \"x\":-1294.433}', '{"x":-1289.917, "y":449.541, "z":96.902}', '{\"y\":446.322, \"z\":96.899, \"x\":-1289.642}', '{\"y\":455.453, \"z\":96.517, \"x\":-1298.851}', '[]', NULL, 1, 1, 0, '{\"x\":-1287.306, \"y\":455.901, \"z\":89.294}', 1500000),
-	(9, 'HillcrestAvenue2874', '2874 Hillcrest Avenue', '{\"x\":-853.346, \"y\":696.678, \"z\":147.782}', '{\"y\":690.875, \"z\":151.86, \"x\":-859.961}', '{\"y\":688.361, \"z\":151.857, \"x\":-859.395}', '{\"y\":701.628, \"z\":147.773, \"x\":-855.007}', '[]', NULL, 1, 1, 0, '{\"x\":-858.543, \"y\":697.514, \"z\":144.253}', 1500000),
-	(10, 'HillcrestAvenue2868', '2868 Hillcrest Avenue', '{\"y\":620.494, \"z\":141.588, \"x\":-752.82}', '{\"y\":618.62, \"z\":143.153, \"x\":-759.317}', '{\"y\":617.629, \"z\":143.153, \"x\":-760.789}', '{\"y\":621.281, \"z\":141.254, \"x\":-750.919}', '[]', NULL, 1, 1, 0, '{\"x\":-762.504, \"y\":618.992, \"z\":135.53}', 1500000),
-	(11, 'TinselTowersApt12', 'Tinsel Towers, Apt 42', '{\"y\":37.025, \"z\":42.58, \"x\":-618.299}', '{\"y\":58.898, \"z\":97.2, \"x\":-603.301}', '{\"y\":58.941, \"z\":97.2, \"x\":-608.741}', '{\"y\":30.603, \"z\":42.524, \"x\":-620.017}', '[]', NULL, 1, 1, 0, '{\"x\":-622.173, \"y\":54.585, \"z\":96.599}', 1700000),
-	(12, 'MiltonDrive', 'Milton Drive', '{\"x\":-775.17, \"y\":312.01, \"z\":84.658}', NULL, NULL, '{\"x\":-775.346, \"y\":306.776, \"z\":84.7}', '[]', NULL, 0, 0, 1, NULL, 0),
-	(13, 'Modern1Apartment', 'Modern Apartment 1', NULL, '{\"x\":-784.194, \"y\":323.636, \"z\":210.997}', '{\"x\":-779.751, \"y\":323.385, \"z\":210.997}', NULL, '[\"apa_v_mp_h_01_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-766.661, \"y\":327.672, \"z\":210.396}', 1300000),
-	(14, 'Modern2Apartment', 'Modern Apartment 2', NULL, '{\"x\":-786.8663, \"y\":315.764, \"z\":186.913}', '{\"x\":-781.808, \"y\":315.866, \"z\":186.913}', NULL, '[\"apa_v_mp_h_01_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-795.735, \"y\":326.757, \"z\":186.313}', 1300000),
-	(15, 'Modern3Apartment', 'Modern Apartment 3', NULL, '{\"x\":-774.012, \"y\":342.042, \"z\":195.686}', '{\"x\":-779.057, \"y\":342.063, \"z\":195.686}', NULL, '[\"apa_v_mp_h_01_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-765.386, \"y\":330.782, \"z\":195.08}', 1300000),
-	(16, 'Mody1Apartment', 'Mody Apartment 1', NULL, '{\"x\":-784.194, \"y\":323.636, \"z\":210.997}', '{\"x\":-779.751, \"y\":323.385, \"z\":210.997}', NULL, '[\"apa_v_mp_h_02_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-766.615, \"y\":327.878, \"z\":210.396}', 1300000),
-	(17, 'Mody2Apartment', 'Mody Apartment 2', NULL, '{\"x\":-786.8663, \"y\":315.764, \"z\":186.913}', '{\"x\":-781.808, \"y\":315.866, \"z\":186.913}', NULL, '[\"apa_v_mp_h_02_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-795.297, \"y\":327.092, \"z\":186.313}', 1300000),
-	(18, 'Mody3Apartment', 'Mody Apartment 3', NULL, '{\"x\":-774.012, \"y\":342.042, \"z\":195.686}', '{\"x\":-779.057, \"y\":342.063, \"z\":195.686}', NULL, '[\"apa_v_mp_h_02_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-765.303, \"y\":330.932, \"z\":195.085}', 1300000),
-	(19, 'Vibrant1Apartment', 'Vibrant Apartment 1', NULL, '{\"x\":-784.194, \"y\":323.636, \"z\":210.997}', '{\"x\":-779.751, \"y\":323.385, \"z\":210.997}', NULL, '[\"apa_v_mp_h_03_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-765.885, \"y\":327.641, \"z\":210.396}', 1300000),
-	(20, 'Vibrant2Apartment', 'Vibrant Apartment 2', NULL, '{\"x\":-786.8663, \"y\":315.764, \"z\":186.913}', '{\"x\":-781.808, \"y\":315.866, \"z\":186.913}', NULL, '[\"apa_v_mp_h_03_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-795.607, \"y\":327.344, \"z\":186.313}', 1300000),
-	(21, 'Vibrant3Apartment', 'Vibrant Apartment 3', NULL, '{\"x\":-774.012, \"y\":342.042, \"z\":195.686}', '{\"x\":-779.057, \"y\":342.063, \"z\":195.686}', NULL, '[\"apa_v_mp_h_03_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-765.525, \"y\":330.851, \"z\":195.085}', 1300000),
-	(22, 'Sharp1Apartment', 'Sharp Apartment 1', NULL, '{\"x\":-784.194, \"y\":323.636, \"z\":210.997}', '{\"x\":-779.751, \"y\":323.385, \"z\":210.997}', NULL, '[\"apa_v_mp_h_04_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-766.527, \"y\":327.89, \"z\":210.396}', 1300000),
-	(23, 'Sharp2Apartment', 'Sharp Apartment 2', NULL, '{\"x\":-786.8663, \"y\":315.764, \"z\":186.913}', '{\"x\":-781.808, \"y\":315.866, \"z\":186.913}', NULL, '[\"apa_v_mp_h_04_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-795.642, \"y\":326.497, \"z\":186.313}', 1300000),
-	(24, 'Sharp3Apartment', 'Sharp Apartment 3', NULL, '{\"x\":-774.012, \"y\":342.042, \"z\":195.686}', '{\"x\":-779.057, \"y\":342.063, \"z\":195.686}', NULL, '[\"apa_v_mp_h_04_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-765.503, \"y\":331.318, \"z\":195.085}', 1300000),
-	(25, 'Monochrome1Apartment', 'Monochrome Apartment 1', NULL, '{\"x\":-784.194, \"y\":323.636, \"z\":210.997}', '{\"x\":-779.751, \"y\":323.385, \"z\":210.997}', NULL, '[\"apa_v_mp_h_05_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-766.289, \"y\":328.086, \"z\":210.396}', 1300000),
-	(26, 'Monochrome2Apartment', 'Monochrome Apartment 2', NULL, '{\"x\":-786.8663, \"y\":315.764, \"z\":186.913}', '{\"x\":-781.808, \"y\":315.866, \"z\":186.913}', NULL, '[\"apa_v_mp_h_05_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-795.692, \"y\":326.762, \"z\":186.313}', 1300000),
-	(27, 'Monochrome3Apartment', 'Monochrome Apartment 3', NULL, '{\"x\":-774.012, \"y\":342.042, \"z\":195.686}', '{\"x\":-779.057, \"y\":342.063, \"z\":195.686}', NULL, '[\"apa_v_mp_h_05_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-765.094, \"y\":330.976, \"z\":195.085}', 1300000),
-	(28, 'Seductive1Apartment', 'Seductive Apartment 1', NULL, '{\"x\":-784.194, \"y\":323.636, \"z\":210.997}', '{\"x\":-779.751, \"y\":323.385, \"z\":210.997}', NULL, '[\"apa_v_mp_h_06_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-766.263, \"y\":328.104, \"z\":210.396}', 1300000),
-	(29, 'Seductive2Apartment', 'Seductive Apartment 2', NULL, '{\"x\":-786.8663, \"y\":315.764, \"z\":186.913}', '{\"x\":-781.808, \"y\":315.866, \"z\":186.913}', NULL, '[\"apa_v_mp_h_06_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-795.655, \"y\":326.611, \"z\":186.313}', 1300000),
-	(30, 'Seductive3Apartment', 'Seductive Apartment 3', NULL, '{\"x\":-774.012, \"y\":342.042, \"z\":195.686}', '{\"x\":-779.057, \"y\":342.063, \"z\":195.686}', NULL, '[\"apa_v_mp_h_06_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-765.3, \"y\":331.414, \"z\":195.085}', 1300000),
-	(31, 'Regal1Apartment', 'Regal Apartment 1', NULL, '{\"x\":-784.194, \"y\":323.636, \"z\":210.997}', '{\"x\":-779.751, \"y\":323.385, \"z\":210.997}', NULL, '[\"apa_v_mp_h_07_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-765.956, \"y\":328.257, \"z\":210.396}', 1300000),
-	(32, 'Regal2Apartment', 'Regal Apartment 2', NULL, '{\"x\":-786.8663, \"y\":315.764, \"z\":186.913}', '{\"x\":-781.808, \"y\":315.866, \"z\":186.913}', NULL, '[\"apa_v_mp_h_07_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-795.545, \"y\":326.659, \"z\":186.313}', 1300000),
-	(33, 'Regal3Apartment', 'Regal Apartment 3', NULL, '{\"x\":-774.012, \"y\":342.042, \"z\":195.686}', '{\"x\":-779.057, \"y\":342.063, \"z\":195.686}', NULL, '[\"apa_v_mp_h_07_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-765.087, \"y\":331.429, \"z\":195.123}', 1300000),
-	(34, 'Aqua1Apartment', 'Aqua Apartment 1', NULL, '{\"x\":-784.194, \"y\":323.636, \"z\":210.997}', '{\"x\":-779.751, \"y\":323.385, \"z\":210.997}', NULL, '[\"apa_v_mp_h_08_a\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-766.187, \"y\":328.47, \"z\":210.396}', 1300000),
-	(35, 'Aqua2Apartment', 'Aqua Apartment 2', NULL, '{\"x\":-786.8663, \"y\":315.764, \"z\":186.913}', '{\"x\":-781.808, \"y\":315.866, \"z\":186.913}', NULL, '[\"apa_v_mp_h_08_c\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-795.658, \"y\":326.563, \"z\":186.313}', 1300000),
-	(36, 'Aqua3Apartment', 'Aqua Apartment 3', NULL, '{\"x\":-774.012, \"y\":342.042, \"z\":195.686}', '{\"x\":-779.057, \"y\":342.063, \"z\":195.686}', NULL, '[\"apa_v_mp_h_08_b\"]', 'MiltonDrive', 0, 1, 0, '{\"x\":-765.287, \"y\":331.084, \"z\":195.086}', 1300000),
-	(37, 'IntegrityWay', '4 Integrity Way', '{\"x\":-47.804, \"y\":-585.867, \"z\":36.956}', NULL, NULL, '{\"x\":-54.178, \"y\":-583.762, \"z\":35.798}', '[]', NULL, 0, 0, 1, NULL, 0),
-	(38, 'IntegrityWay28', '4 Integrity Way - Apt 28', NULL, '{\"x\":-31.409, \"y\":-594.927, \"z\":79.03}', '{\"x\":-26.098, \"y\":-596.909, \"z\":79.03}', NULL, '[]', 'IntegrityWay', 0, 1, 0, '{\"x\":-11.923, \"y\":-597.083, \"z\":78.43}', 1700000),
-	(39, 'IntegrityWay30', '4 Integrity Way - Apt 30', NULL, '{\"x\":-17.702, \"y\":-588.524, \"z\":89.114}', '{\"x\":-16.21, \"y\":-582.569, \"z\":89.114}', NULL, '[]', 'IntegrityWay', 0, 1, 0, '{\"x\":-26.327, \"y\":-588.384, \"z\":89.123}', 1700000),
-	(40, 'DellPerroHeights', 'Dell Perro Heights', '{\"x\":-1447.06, \"y\":-538.28, \"z\":33.74}', NULL, NULL, '{\"x\":-1440.022, \"y\":-548.696, \"z\":33.74}', '[]', NULL, 0, 0, 1, NULL, 0),
-	(41, 'DellPerroHeightst4', 'Dell Perro Heights - Apt 28', NULL, '{\"x\":-1452.125, \"y\":-540.591, \"z\":73.044}', '{\"x\":-1455.435, \"y\":-535.79, \"z\":73.044}', NULL, '[]', 'DellPerroHeights', 0, 1, 0, '{\"x\":-1467.058, \"y\":-527.571, \"z\":72.443}', 1700000),
-	(42, 'DellPerroHeightst7', 'Dell Perro Heights - Apt 30', NULL, '{\"x\":-1451.562, \"y\":-523.535, \"z\":55.928}', '{\"x\":-1456.02, \"y\":-519.209, \"z\":55.929}', NULL, '[]', 'DellPerroHeights', 0, 1, 0, '{\"x\":-1457.026, \"y\":-530.219, \"z\":55.937}', 1700000)
-;
 
 CREATE TABLE `billing` (
 	`id` INT NOT NULL AUTO_INCREMENT,
@@ -540,7 +467,7 @@ CREATE TABLE `vehicles` (
 	`category` VARCHAR(60) DEFAULT NULL,
 
 	PRIMARY KEY (`model`)
-);
+) ENGINE=InnoDB;
 
 INSERT INTO `vehicles` (name, model, price, category) VALUES
 ('Adder', 'adder', 365000, 'super'),
@@ -1021,4 +948,8 @@ CREATE TABLE IF NOT EXISTS `faction_grades` (
 ) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 INSERT INTO `faction_grades` (`id`, `faction_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES (1, 'nofaction', 0, 'nofaction', 'Sans faction', 0, '{}', '{}');
+
+
+ALTER TABLE `users`
+  ADD UNIQUE KEY `id` (`id`);
 
